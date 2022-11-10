3981bdf64e3b0d388a86e92adc54f6b7995c479b
Mike Repec <mikerepec@gmail.com>
2022-11-10 01:51:00 +0100

    Display item recommendations

--- src/components/ChampionAvatar.vue
+++ src/components/ChampionAvatar.vue
@@ -1,5 +1,9 @@
 <template>
-  <b-tooltip :label="`${champion.name} - ${champion.cost}g`" type="is-light">
+  <b-tooltip
+    :label="`${champion.name} - ${champion.cost}g`"
+    :active="mainTooltipActive"
+    type="is-light"
+  >
     <div
       class="champion-avatar"
       :class="{ selected, [`c${champion.cost}`]: true }"
@@ -7,7 +11,24 @@
     >
       <b-icon v-if="cancellable" class="close-icon" icon="close" size="is-small" />
       <b-icon v-if="selected" class="selected-icon" icon="check" size="is-small" />
-      <img :src="champion.image" :alt="champion.name" :title="champion.name" />
+      <img class="avatar-image" :src="champion.image" :alt="champion.name" :title="champion.name" />
+
+      <div class="item-images">
+        <b-tooltip
+          v-for="(item, i) in champion.items"
+          :key="`${item.name}-${i}`"
+          :label="`${item.name}`"
+          type="is-light"
+        >
+          <img
+            :src="item.image"
+            :alt="item.name"
+            @mouseover="mainTooltipActive = false"
+            @mouseout="mainTooltipActive = true"
+            @click.stop
+          />
+        </b-tooltip>
+      </div>
     </div>
   </b-tooltip>
 </template>
@@ -18,10 +39,15 @@ export default {
     champion: {
       type: Object,
       required: true,
-      validator: (c) => c.image && c.name,
+      validator: c => c.image && c.name,
     },
     cancellable: Boolean,
     selected: Boolean,
+  },
+  data() {
+    return {
+      mainTooltipActive: true,
+    }
   },
 }
 </script>
@@ -35,7 +61,7 @@ export default {
   border-radius: 3px;
 
   &:not(.selected):hover {
-    img {
+    .image {
       transform: scale(1.1);
     }
 
@@ -44,13 +70,30 @@ export default {
     }
   }
 
-  img {
+  .avatar-image {
     display: block;
     width: 100%;
     max-width: 100px;
     height: 100%;
     transition: transform 0.2s;
     border-radius: 1px;
+  }
+
+  .item-images {
+    position: absolute;
+    bottom: 0px;
+    left: 0px;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+
+    img {
+      width: 18px;
+      border-width: 1px;
+      border-style: solid;
+      border-radius: 2px;
+      border-color: #666;
+    }
   }
 
   .close-icon {
