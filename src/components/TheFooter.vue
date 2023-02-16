d4b265c1be1718fdf70dd862ec52dba18e7236f0
Mike Repeć <mikerepec@gmail.com>
2023-02-16 21:44:28 +0100

    Update footer link

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div class="has-text-centered has-text-grey-light">
       <small>
-        &copy; {{ year }} TFT Suggester. Made by <a href="https://flexicondev.com/">Mike</a>. All
+        &copy; {{ year }} TFT Suggester. Made by <a href="https://flexicon.dev/">Mike</a>. All
         Rights Reserved.
         <br />
         This site is not affiliated with Riot Games.
